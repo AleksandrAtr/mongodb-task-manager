10913bd6909fbf205f22534317ad8ca43ea648a7
AleksandrAtr <atraleks@gmail.com>
2023-12-29 07:21:30 +0000

    add: Task form input fields

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,17 @@
-$(document).ready(function(){
-    $(".sidenav").sidenav({edge:"right"});
-    $('.collapsible').collapsible();
-  });
+/*
+    jQuery for MaterializeCSS initialization
+*/
+
+$(document).ready(function () {
+    $(".sidenav").sidenav({edge: "right"});
+    $(".collapsible").collapsible();
+    $(".tooltipped").tooltip();
+    $(".datepicker").datepicker({
+        format: "dd mmmm, yyyy",
+        yearRange: 3,
+        showClearBtn: true,
+        i18n: {
+            done: "Select"
+        }
+    });
+});
